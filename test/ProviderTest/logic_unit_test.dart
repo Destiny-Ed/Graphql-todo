f86c44bd260590354e6d1e7c547398097deda101
Destiny-Ed <dikeachaeze@gmail.com>
2022-02-27 12:32:33 +0100

    Tested the logic to confirm updating of task

--- test/ProviderTest/logic_unit_test.dart
+++ test/ProviderTest/logic_unit_test.dart
@@ -3,12 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:joovlin/GrapqhqlSchema/todo_schema.dart';
-import 'package:joovlin/Provider/Mutations/add_todo_provider.dart';
 import 'package:joovlin/Provider/client_provider.dart';
 import 'package:joovlin/Utils/url.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:provider/provider.dart';
 
 /// Mocks a callback function on which you can use verify
 class MockHttpClient extends Mock implements http.Client, BuildContext {}
@@ -41,12 +39,46 @@ void main() async {
     });
 
     test("Check for exception", () async {
-
       QueryResult value = await client!.value.mutate(
         MutationOptions(document: gql(TaskSchema.addTaskSchema), variables: {
           "description": "Helloworld",
           "developer_id": 2,
           "title": "Hi"
+        }),
+      );
+
+      expect(value.hasException, true);
+    });
+  });
+
+  ///Test Update Task
+  group('Update Task', () {
+    test("Return true if task was updated", () async {
+      // final provider = Provider.of<AddTaskProvider>(MockHttpClient());
+
+      QueryResult value = await client!.value.mutate(
+        MutationOptions(document: gql(TaskSchema.updateTaskSchema), variables: {
+          "id": '39db3d54-e178-46c5-b9a0-e528d4c874e3',
+          "payload": {
+            "isCompleted": true,
+            "title": "title",
+            "description": "description"
+          }
+        }),
+      );
+
+      expect(value.hasException, false);
+    });
+
+    test("Check for exception if task was not updated", () async {
+      QueryResult value = await client!.value.mutate(
+        MutationOptions(document: gql(TaskSchema.updateTaskSchema), variables: {
+          "id": '',
+          // "payload": {
+          //   "isCompleted": true,
+          //   "title": "title",
+          //   "description": "description"
+          // }
         }),
       );
 
